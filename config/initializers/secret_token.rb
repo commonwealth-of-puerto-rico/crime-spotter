a43af985359001ad5fac46c81f07074a0112c3cb
Roberto C. Rivera <rivera.roberto89@gmail.com>
2014-02-22 22:37:21 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CrimeSpotter::Application.config.secret_key_base = '6969a5cad5be22791b6535a177f71d91da23a587660bd884e8ba3b2b45ca01dccdd0881e320a0ab613ff425ee8b8ac8b3e442d5bc8871a96ae3499b4d78f28a3'
