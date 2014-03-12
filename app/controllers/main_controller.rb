488505aeaff7bf1b8c1a3ed9f7c13fa58562aa25
Andrés Colón <levipr@gmail.com>
2014-03-11 20:36:32 -0400

    Deleting old files

--- app/controllers/main_controller.rb
+++
@@ -1,9 +0,0 @@
-require 'open-uri'
-require 'json'
-
-class MainController < ApplicationController
-
-  def index
-  	@data = JSON.parse(open("http://compass.pr.gov/compass/delitos/location?lnglat=-66.05898857116699,18.427990549260358&dist=0.1").read)
-  end
-end
--- config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-CrimeSpotter::Application.config.session_store :cookie_store, key: '_crime-spotter_session'
