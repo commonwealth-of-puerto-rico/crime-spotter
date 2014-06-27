bd2c4cbd4b9d0fe2fcbf5cea1a2afd67e72bb175
Roberto C. Rivera <roberto@devstud.io>
2014-06-27 00:34:13 -0400

    Removed is_geojson parameter from request

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -326,20 +326,8 @@ var fires = L.geoJson(null, {
 
 
 var markers = L.markerClusterGroup();
-var crime_collection_request = "http://crimenes-api.herokuapp.com/crimes?polygon=[[-64.500732421875,19.06990562064469],[-68.01361083984375,19.06990562064469],[-68.01361083984375,17.368988699356095],[-64.500732421875,17.368988699356095]]&from_date=2013-01-01&to_date=2014-04-22&is_geojson=true";
+var crime_collection_request = "http://crimenes-api.herokuapp.com/crimes?polygon=[[-64.500732421875,19.06990562064469],[-68.01361083984375,19.06990562064469],[-68.01361083984375,17.368988699356095],[-64.500732421875,17.368988699356095]]&from_date=2013-01-01&to_date=2014-04-22";
 
-/*
-var geoData = function(data) {
-  // GeoData extraction algorithm
-  var geoData = [];
-  _.map(geoData, function(data) {
-    _.map(data["features"], function(geometry) {
-      var coords = _.map(geometry, "coordinates");
-      geoData.push(coords[1]);
-    });
-  }
-}
-*/
 var heatmap = L.heatLayer(null, {radius: 20, blur:20, max:0.4,maxZoom:18});
 heatmap.setLatLngs([]);
 $.getJSON(crime_collection_request, function (data) {
